dfb6a3d518104225d715cca3eca785cf81d17cc8
Matt Slocum <mslocum@spotx.tv>
2018-06-26 21:57:13 -0600

    Finish InsertImage Widget

--- src/plugins/InsertImage.js
+++ src/plugins/InsertImage.js
@@ -10,21 +10,30 @@ export class InsertImage extends Plugin {
   init() {
     const editor = this.editor;
 
-    editor.ui.componentFactory.add( 'insertImage', locale => {
+    editor.ui.componentFactory.add('insertImage', locale => {
       const view = new ButtonView( locale );
 
-      view.set( {
+      view.set({
         label: 'Insert image',
         icon: imageIcon,
         tooltip: true
-      } );
+      });
 
       // Callback executed once the image is clicked.
       view.on( 'execute', () => {
-        const imageURL = prompt( 'Image URL' );
-      } );
+        const imageUrl = prompt( 'Image URL' );
+
+        editor.model.change( writer => {
+          const imageElement = writer.createElement( 'image', {
+            src: imageUrl
+          });
+
+          // Insert the image in the current selection location.
+          editor.model.insertContent( imageElement, editor.model.document.selection );
+        });
+      });
 
       return view;
-    } );
+    });
   }
 }
